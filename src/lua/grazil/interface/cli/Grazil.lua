88365c218b8eb275da81c43043041d7e2fec1151
Till Tantau <tantau@Tills-MacBook-Pro-2.local>
2016-08-10 16:21:35 +0200

    initial

--- src/lua/grazil/interface/cli/Grazil.lua
+++ src/lua/grazil/interface/cli/Grazil.lua
@@ -9,14 +9,21 @@ Vertex  = require 'grazil.model.Vertex'
 Arc     = require 'grazil.model.Arc'
 CoreBuilder = require 'grazil.build.CoreBuilder'
 
+ans = {}
+
 while true do
 
   local line = io.read ()
 
-  local fun = load (line)
+  local fun = load ("ans[" .. #ans+1 .. "] = (" .. line .. ")")
 
-  if not pcall(fun) then
-     print("Unknown command: " .. line)
+  local x = pcall(fun)
+  ans[#ans+1] = x
+
+  if not x then
+    print("Unknown command: " .. line)
+  else
+    print("ans[" .. #ans .. "] = " .. tostring(ans[#ans]))
   end
   
 end
